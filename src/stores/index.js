2302918934356c280292b968a5ef7400f3c5f3c1
yogesh <yogesh.chaudhari@yaaman.com>
2018-08-30 14:26:51 +0530

    Refactoring - Updated file name for root saga

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './reducers';
-import rootSaga from '../sagas/root';
+import rootSaga from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
